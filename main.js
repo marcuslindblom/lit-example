22005e1b6e378f66080c26fc73dce1878e4c5818
Marcus Lindblom <marcus@wieldy.app>
2022-05-25 22:13:18 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+import { LitElement, html } from "https://cdn.skypack.dev/lit@2.2.4";
+import { Router } from "https://cdn.skypack.dev/@lit-labs/router@0.0.2";
+
+
+class One extends LitElement {
+  render() {
+    return html`<h1>Hello</h1><a href="/world">World</a>`;
+  }
+}
+
+customElements.define("view-one", One);
+
+class Two extends LitElement {
+  render() {
+    return html`<h1>World</h1>`;
+  }
+}
+
+customElements.define('view-two', Two);
+
+class App extends LitElement {
+  router = new Router(this, [
+    {
+      path: '/*',
+      render: () => html`<view-one></view-one>`,
+    },
+    {
+      path: '/world',
+      render: () => html`<view-two></view-two>`,      
+    }
+  ]);
+  render() {
+    return html`${this.router.outlet()}`;
+  }
+}
+
+customElements.define('view-app', App);
